8410e80b4323d84420009b8d35eed2bae0ba44d8
RSPrasanth <75479170+RSPrasanth@users.noreply.github.com>
2021-01-30 12:58:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var gameState = 0;
+var canvas;
+var contestantCount;
+var quiz, question, contestant;
+var allContestants;
+var database;
+var options, option1, option2, option3, option4;
+
+function preload(){
+  
+}
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+
+  
+}
